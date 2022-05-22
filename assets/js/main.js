f205af6c8756d552ca87f55cbe707c9719de00f6
Jeremy <jeremynght@icloud.com>
2022-05-22 17:45:51 +0200

    Must Have Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -153,6 +153,37 @@
   }
 
   /**
+   * Isotope et filtre du portefolio
+   */
+  window.addEventListener('load', () => {
+    let portfolioContainer = select('.portfolio-container');
+    if (portfolioContainer) {
+      let portfolioIsotope = new Isotope(portfolioContainer, {
+        itemSelector: '.portfolio-item'
+      });
+
+      let portfolioFilters = select('#portfolio-flters li', true);
+
+      on('click', '#portfolio-flters li', function(e) {
+        e.preventDefault();
+        portfolioFilters.forEach(function(el) {
+          el.classList.remove('filter-active');
+        });
+        this.classList.add('filter-active');
+
+        portfolioIsotope.arrange({
+          filter: this.getAttribute('data-filter')
+        });
+        portfolioIsotope.on('arrangeComplete', function() {
+          AOS.refresh()
+        });
+      }, true);
+    }
+
+  });
+
+
+  /**
    * Lancer la lightbox du portefeuille
    */
   const portfolioLightbox = GLightbox({
@@ -166,7 +197,7 @@
     speed: 400,
     loop: true,
     autoplay: {
-      delay: 5000,
+      delay: 2000,
       disableOnInteraction: false
     },
     pagination: {
